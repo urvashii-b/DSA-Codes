2252247e7b034bf04fdba6ba1a388111605b347f
Urvashi Bhargava <urvashi.officialcse@gmail.com>
2024-05-09 10:50:51 +0530

    sorting algos - 1

--- sorting.cpp
+++ sorting.cpp
@@ -1,36 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-/* Selection Sort - select the minimum and swap with i
+/* Selection Sort - select the minimum and swap with i: O(n^2)
 13 4 7 9 -> (1) 4 13 7 9 -> (2) 4 7 13 9 -> (3) 4 7 9 13 : 4 elements sorted in 3 steps 
 
 Bubble Sort - compare adjacent elements and swap
+worst - O(n^2), best - O(n)
+accessing an index which is not present - runtime error
 
 Insertion Sort - insert the element in the correct position in the sorted array
+worst - O(n^2), best - O(n)
 */
 
 void selectionSort(int arr[], int n){
     for(int i=0;i<=n-2;i++){
-        int min = i;
+        int mini = i;
         for(int j=i;j<=n-1;j++){
-            if(arr[j]<arr[min]){
-                min = j;
+            if(arr[j]<arr[mini]){
+                mini = j;
             }
         }
-        swap(arr[i],arr[min]);
+        int temp = arr[i];
+        arr[i]= arr[mini];
+        arr[mini]= temp;
     }
 }
 
 void bubbleSort(int arr[], int n){
-    for(int i=n-1;i>=1;i++){
+    for(int i=n-1;i>=0;i--){
         int didSwap = 0;
         for(int j=0;j<=i-1;j++){
             if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+                int temp = arr[j+1];
+                arr[j+1]= arr[j];
+                arr[j]= temp;
                 didSwap = 1;
             }
         }
-        if(didSwap==1){
+        if(didSwap==0){
             break;
         }
     }
@@ -38,9 +45,11 @@ void bubbleSort(int arr[], int n){
 
 void insertionSort(int arr[], int n){
     for(int i=0;i<=n-1;i++){
-        int j=i;
+        int j = i;
         while(j>0 && arr[j-1]>arr[j]){
-            swap(arr[j-1],arr[j]);
+            int temp = arr[j-1];
+            arr[j-1]= arr[j];
+            arr[j]= temp;
             j--;
         }
     }
@@ -57,7 +66,7 @@ int main(){
     cin>>n;
     int arr[n];
     for(int i=0;i<n;i++) cin>>arr[i];
-    insertionSort(arr,n);
+    bubbleSort(arr,n);
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
